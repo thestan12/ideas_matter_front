387a68a0ddea3414aaea041076cd2800295a4b8b
Moxa <mohaxoy98@gmail.com>
2020-04-14 10:59:35 +0200

    debuging freez problem

--- quasar.conf.js
+++ quasar.conf.js
@@ -42,7 +42,7 @@ module.exports = function (ctx) {
 
     // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
-      vueRouterMode: 'hash', // available values: 'hash', 'history'
+      vueRouterMode: 'history', // available values: 'hash', 'history'
       env: ctx.dev
         ? { // so on dev we'll have
           API: JSON.stringify('https://jeeback.azurewebsites.net')
